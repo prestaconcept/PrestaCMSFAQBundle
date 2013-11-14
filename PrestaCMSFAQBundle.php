0f379c7a0ec7fcd9df57e80e60c49348e0e2cec6
Nicolas Bastien <nbastien@prestaconcept.net>
2013-11-14 11:32:00 +0100

    Bundle set up

---
+++ PrestaCMSFAQBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PrestaCMSFAQBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSFAQBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaCMSFAQBundle extends Bundle
+{
+}
